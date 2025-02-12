93c9364b20fb33734d7c73579c0c77c936082ab7
Laura  Melissa <laneirab@unal.edu.co>
2025-02-12 01:05:51 -0500

    Cambios en responsive y links en íconos

--- src/Components/navBar.tsx
+++ src/Components/navBar.tsx
@@ -1,19 +1,18 @@
 import Container from "react-bootstrap/Container";
 import Nav from "react-bootstrap/Nav";
 import Navbar from "react-bootstrap/Navbar";
-import NavDropdown from "react-bootstrap/NavDropdown";
-
-import navBar from "../styles/navBar.module.css";	
-
+import React from "react";
+//import NavDropdown from "react-bootstrap/NavDropdown";
 
 
 export const NavBar: React.FC = () => {
+
   return (
     <Navbar fixed="top" expand="lg" className="navBar">
       <Container>
       <Navbar.Brand href="#">
         <img
-        src="src\Images\VGMRecurso 4.svg"
+        src="src\assets\logos\logoBlancoSinTexto.svg"
         width="30"
         height="30"
         className="d-inline-block align-top"
@@ -21,29 +20,29 @@ export const NavBar: React.FC = () => {
         />
       </Navbar.Brand>
       <Navbar.Toggle aria-controls="navbar-nav" />
-      <Navbar.Collapse id="navbar-nav">
+      <Navbar.Collapse id="navbar-nav" className="custom-collapse">
         <Nav className="ms-auto"> {/* Alinea a la derecha */}
-        <Nav.Link href="#facebook">
+        <Nav.Link href="https://m.facebook.com/profile.php?id=453888817815565" target="_blank" rel="noopener noreferrer">
           <img
-          src="src\Images\VGMRecurso 7.svg"
+          src="src\assets\logos\logoFacebook.svg"
           width="20"
           height="20"
           className="d-inline-block align-top"
           alt="Facebook"
           />
         </Nav.Link>
-        <Nav.Link href="#instagram">
+        <Nav.Link href="https://www.instagram.com/vmggroup_?igsh=MXM2NHJkeHNvanBocw==" target="_blank" rel="noopener noreferrer">
           <img
-          src="src\Images\VGMRecurso 8.svg"
+          src="src\assets\logos\logoInstagram.svg"
           width="20"
           height="20"
           className="d-inline-block align-top"
           alt="instagram"
           />
         </Nav.Link>
-        <Nav.Link href="#mail">
+        <Nav.Link href="mailto:vgmgrouplit@gmail.com" target="_blank" rel="noopener noreferrer">
           <img
-          src="src\Images\VGMRecurso 9.svg"
+          src="src\assets\logos\logoCorreo.svg"
           width="20"
           height="20"
           className="d-inline-block align-top"
--- src/Pages/App.tsx
+++ src/Pages/App.tsx
@@ -11,7 +11,7 @@ function App() {
     <BlurBackground />
     <NavBar />
       <div className='logo'>
-        <a href="https://react.dev" target="_blank">
+        <a >
           <IconoBlancoTexto />
         </a>
       </div>
@@ -19,9 +19,11 @@ function App() {
       <h3 className="Fin">Financing</h3>
       <h2 className="Future" style={{ fontFamily: 'Beckman' }}>The future</h2>
       </div>
+      <a href="https://wa.me/13052982118" target="_blank" rel="noopener noreferrer"> 
         <Button style={{ backgroundColor: '#29275C', borderColor: '#29275C', fontFamily: 'Beckman, sans-serif', fontSize: '15px', padding: '15px' ,margin:"30px"}} variant="primary" size="lg">
           Start Now
         </Button>
+      </a>
       <p className="subtexto">
         Página en construcción
       </p>
