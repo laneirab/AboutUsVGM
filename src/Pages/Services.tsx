7c2cb7a1401a9e2c6451d96e7de82be509534bd3
santiagoperez0 <jperezpi@unal.edu.co>
2025-06-14 02:55:37 -0500

    Son las 5 de la mañana...

--- src/Pages/Services.tsx
+++ src/Pages/Services.tsx
@@ -25,9 +25,10 @@ export const Services: React.FC = () => {
         trigger: container,
         start: 'top top',
         end: () => `+=${totalScrollWidth}`,
-        scrub: true,
         pin: true,
         anticipatePin: 1,
+        scrub: 3,
+        snap : 0.05
       },
     });
 
@@ -41,31 +42,56 @@ export const Services: React.FC = () => {
       <div className="horizontal-container" ref={containerRef}>
         <section className="section section1">
           <div className="content">
-            <h2 className="texto">¿Sabe que Caravana?</h2>
+            <h2 className="texto1">¿Sabe que Caravana?</h2>
           </div>
         </section>
 
         <section className="section section2">
           <div className="content">
+            <img
+        src="public\Fonts\Images\logos\taxes.svg"
+        width="200"
+        height="200"
+        alt="vgm"
+        />
             <h2 className="texto">Si no le gusta el motilado Pailaaa</h2>
+
           </div>
         </section>
 
         <section className="section section3">
           <div className="content">
-            <h2 className="texto">No lo mire , yo me siento bien asi</h2>
+            <img
+        src="public\Fonts\Images\logos\consulting.svg"
+        width="200"
+        height="200"
+        alt="vgm"
+        />
+            <h2 className="texto1">No lo mire , yo me siento bien asi</h2>
           </div>
         </section>
 
         <section className="section section4">
           <div className="content">
-            <h2 className="texto">Yo no me mantengo pendiente del que diran</h2>
+            <img
+        src="public\Fonts\Images\logos\health.svg"
+        width="200"
+        height="200"
+        alt="vgm"
+        />
+            <h2 className="texto1">Yo no me mantengo pendiente del que diran</h2>
           </div>
         </section>
 
         <section className="section section5">
           <div className="content">
-            <h2 className="texto"><strong>Desde que yo este bien , que ruede el mundo a mi alrededor ¿listo?</strong></h2>
+            <img
+        src="public\Fonts\Images\logos\real.svg"
+        width="200"
+        height="200"
+        alt="vgm"
+        />
+            <h2 className="texto1"><strong>Desde que yo este bien , que ruede el mundo a mi alrededor ¿listo?</strong></h2>
           </div>
         </section>
       </div>
